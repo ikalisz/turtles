f3ed2f260c7c9c5087175767fbe942a0d8a95430
ikalisz <ikalisz@yahoo.com>
2023-02-16 14:01:53 -0700

    added finding functions

--- helpers/fuel.lua
+++ helpers/fuel.lua
@@ -7,12 +7,28 @@ function Fuel.checkFuel(originalSlot)
     if (fuelLevel < 50) then
         -- Implement logic to determine where to place the chest
         -- Implement logic to determine where the chest is in the inventory
-        turtle.select(1)
+        Fuel.findChest()
         turtle.placeUp()
         local chest = peripheral.wrap("top")
         chest.pushItems("down", 1, 64)
         turtle.refuel()
         turtle.digUp()
+    end
+end
+
+function Fuel.findChest()
+    -- This function will sort through the inventory and return the slot the seeds are in
+    -- LONG TERM: if out of seeds will request more seeds
+    local selectedData = turtle.getItemDetail()
+    if (string.find(data.name, "ender_storage")) then
+        return
+    end
+    for i = 16, 1, -1 do
+        local data = turtle.getItemDetail(i)
+        if (string.find(data.name, "ender_storage")) then
+            select (i)
+            break
+        end
     end
 end
 
--- programs/farming/farming.lua
+++ programs/farming/farming.lua
@@ -2,7 +2,7 @@
 -- Change the below variables to your own numbers for your crops
 
 local finishedCropAge = 7 -- This is the finished crop age, you can find this by doing checkBlock.lua on a finished crop
-local columns = 7 -- This is the number of columns in the grid
+local columns = 11 -- This is the number of columns in the grid
 local rows = 13 -- This is the number of rows in the grid
 local checkBlock = require('checkBlockFarm')
 local Farm = {}
@@ -27,21 +27,43 @@ function Farm.checkCrop()
     end
 end
 
+function Farm.findSeeds()
+    -- This function will sort through the inventory and return the slot the seeds are in
+    -- LONG TERM: if out of seeds will request more seeds
+    local selectedData = turtle.getItemDetail()
+    if (string.find(data.name, "seeds")) then
+        return
+    end
+    for i = 16, 1, -1 do
+        local data = turtle.getItemDetail(i)
+        if (string.find(data.name, "seed")) then
+            select (i)
+            break
+        end
+    end
+end
+
 function Farm.nextCrop()
     -- Need to determine where in the grid the turtle is and turn accordingly
     -- If Y is maxed, we are at the last row, need to see if we are at the top or bottom
+    print("Current X Position: ", tostring(curXPos))
+    print("Current Y Position: ", tostring(curYPos))
     if (curXPos == endXPos and curYPos == endYPos) then
+        if ((curXPos % 2) ~= 0) then
+            turtle.turnRight()
+            turtle.turnRight()
+        end
         Move.reset(curXPos, curYPos)
         curXPos = 1
         curYPos = 1
         return
-    elseif (curYPos == rows) then
+    elseif (curYPos == rows and (curXPos % 2) ~= 0) then
         turtle.turnRight()
         Move.forward(1)
         turtle.turnRight()
         curXPos = curXPos + 1
         return
-    elseif (curYPos == 1 and curXPos ~= 1) then
+    elseif (curYPos == 1 and curXPos ~= 1 and (curXPos % 2) == 0) then
         turtle.turnLeft()
         Move.forward(1)
         turtle.turnLeft()
@@ -62,18 +84,17 @@ function Farm.farmCrop()
     if (didDig) then
         -- Need to add findSeeds method that will find which inventory slot seeds are in
         turtle.suckDown()
-        turtle.select(2)
+        Farm.findSeeds()
         turtle.placeDown()
         turtle.select(1)
     end
 end
 
 function Farm.handleRow()
-    for i = crops, 2, -1 do
+    for i = crops, 1, -1 do
         Farm.checkCrop()
         Farm.nextCrop()
     end
-    Farm.reset()
 end
 
 Farm.handleRow()
